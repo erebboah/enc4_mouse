4b0a45700e0f10c9709260c33aed7589f0cc90f4
Liz Rebboah <erebboah@uci.edu>
2022-08-14 11:16:56 -0700

    update

--- snrna/scripts/integrate_cortex.sh
+++ snrna/scripts/integrate_cortex.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=hc_int    ## Name of the job
+#SBATCH --job-name=cx_int    ## Name of the job
 #SBATCH -A SEYEDAM_LAB            ## account to charge 
 #SBATCH -p standard               ## partition/queue name
 #SBATCH --nodes=1                 ## (-N) number of nodes to use
 #SBATCH --cpus-per-task=8         ## number of cores the job needs
-#SBATCH --output=hc1-%J.out ## output log file
-#SBATCH --error=hc1-%J.err ## error log file
+#SBATCH --output=cx1-%J.out ## output log file
+#SBATCH --error=cx1-%J.err ## error log file
 #SBATCH --mem=128G
 
 source /data/homezvol2/erebboah/miniconda3/bin/activate hpc3sc
 
 mkdir ../seurat
-Rscript integrate_parse_10x.R --tissue hippocampus
+Rscript integrate_parse_10x.R --tissue cortex
